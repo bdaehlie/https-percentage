1c3da58fe5174d6aff74d8e70fbace9bb005b6f4
Josh Aas <jaas@kflag.net>
2018-05-08 21:31:16 -0500

    Progress

--- https-percentage.js
+++ https-percentage.js
@@ -1,2 +1,27 @@
 var totalRequests = 0;
 var httpsRequests = 0;
+
+function updateUI() {
+
+}
+
+function onHTTPRequest(e) {
+  totalRequests++;
+  updateUI();
+}
+
+function onHTTPSRequest(e) {
+  totalRequests++;
+  httpsRequests++;
+  updateUI();
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+  onHTTPRequest,
+  {urls: ["http://*/*"]}
+);
+
+browser.webRequest.onHeadersReceived.addListener(
+  onHTTPSRequest,
+  {urls: ["https://*/*"]}
+);
